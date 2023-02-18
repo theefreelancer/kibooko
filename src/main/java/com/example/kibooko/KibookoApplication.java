88a6183c865d5a218290f6c2a71ecb4fd87e6d75
“ronaldssebalamu” <ronaldssebalamu2@gmail.com>
2023-02-18 11:36:32 +0300

    Booting the application

---
+++ src/main/java/com/example/kibooko/KibookoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kibooko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KibookoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KibookoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kibooko/KibookoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kibooko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KibookoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
