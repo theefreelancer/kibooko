a2830413cb19ff78d4a12ba1a74ddea7833a0116
“ronaldssebalamu” <ronaldssebalamu2@gmail.com>
2023-02-19 11:03:34 +0300

    Added the endpoints and routing functionality

--- src/main/java/com/example/kibooko/KibookoApplication.java
+++ src/main/java/com/example/kibooko/KibookoApplication.java
@@ -2,12 +2,25 @@ package com.example.kibooko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
 public class KibookoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KibookoApplication.class, args);
 	}
+	@GetMapping("/hello")
+			public String sayHello(@RequestParam(value= "myName", defaultValue ="World") String name){
+			return String.format("Hello %s!", name);
+		}
+
 
 }
